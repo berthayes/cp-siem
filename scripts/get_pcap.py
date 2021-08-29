b91f54c74c95d8846047773972292a34b0106e0a
Bert <berthayes@users.noreply.github.com>
2021-08-28 22:06:23 -0500

    Update get_pcap.py

--- scripts/get_pcap.py
+++ scripts/get_pcap.py
@@ -9,9 +9,11 @@ os.rename('./pcaps/zeek_streamer.pcap', './pcaps/zeek_streamer.pcap.bak')
 
 url = 'https://bhayes-pcaps.s3.us-east-2.amazonaws.com/garage-2020-10-18.pcap'
 headers = {'Host': 'bhayes-pcaps.s3.us-east-2.amazonaws.com'}
+print("Downloading pcap file...")
 r = requests.get(url, headers=headers)
 pcap_binary = r.content
 
+print("Writing file to disk...")
 with open("./pcaps/zeek_streamer.pcap", "wb") as pcap:
     pcap.write(pcap_binary)
 
