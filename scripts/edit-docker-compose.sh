ff80158f712ea226e9ccdee9fe8b71cd02653b19
Bert <berthayes@users.noreply.github.com>
2021-08-28 20:04:54 -0500

    Create edit-docker-compose.sh

---
+++ scripts/edit-docker-compose.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Dumb script to edit a template file and replace the ksqldb-server line
+
+# This is a neat trick to find out the public hostname of your EC2 host
+PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+
+
+DOCKER_COMPOSE_FILE="/home/ubuntu/cp-siem/docker-compose.yml"
+
+
+# Run this if you want to (have to) run Confluent Control Center on port 80 and ksqldb server (REST API) on port 443 with no encryption
+#/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/localhost:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$PUBLIC_HOSTNAME':443\"/' -e 's/      - "9021:9021"/      - "80:9021"/' -e 's/      - "8088:8088"/      - "443:8088"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-siem/workshop-docker-compose.yml
+
+
+# Run this if you are sane and can run these services on their default ports (9021 for Confluent Control Center and 8088 for ksqlDB)
+/bin/sed -e 's/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/localhost:8088\"/      CONTROL_CENTER_KSQL_KSQLDB1_ADVERTISED_URL: \"http:\/\/'$PUBLIC_HOSTNAME':8088\"/' $DOCKER_COMPOSE_FILE > /home/ubuntu/cp-siem/workshop-docker-compose.yml
