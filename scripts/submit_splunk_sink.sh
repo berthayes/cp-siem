93c870017167d8e7bf66fb4ba89b6931aaaf626a
Will LaForest <will@confluent.io>
2021-11-16 05:46:34 +0000

    fixed to match working startup script

--- scripts/submit_splunk_sink.sh
+++ scripts/submit_splunk_sink.sh
@@ -3,17 +3,17 @@
 HEADER="Content-Type: application/json"
 DATA=$( cat << EOF
 {
-  "name": "SPLUNKSINK",
+"name": "SPLUNKSINK",
   "config": {
     "confluent.topic.bootstrap.servers": "broker:29092",
-    "name": "CISCO_ASA",
+    "name": "SPLUNKSINK",
     "connector.class": "com.splunk.kafka.connect.SplunkSinkConnector",
     "tasks.max": "1",
     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
     "value.converter": "org.apache.kafka.connect.storage.StringConverter",
     "topics": "CISCO_ASA",
     "splunk.hec.token": "ef16f05f-40e0-4108-a644-5323e02aaa44",
-    "splunk.hec.uri": "https://splunk:8088",
+    "splunk.hec.uri": "https://splunk:8090",
     "splunk.hec.ssl.validate.certs": "false",
     "splunk.hec.json.event.formatted": "true"
   }
